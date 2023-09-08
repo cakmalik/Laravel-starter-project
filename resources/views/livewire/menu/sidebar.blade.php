f57cf0e77c02bb2c0cba1fd26099b1878c31c9d6
cakmalik <hasinilmalik@gmail.com>
2023-09-08 10:04:04 +0700

    add siidebar menu

--- resources/views/livewire/menu/sidebar.blade.php
+++ resources/views/livewire/menu/sidebar.blade.php
@@ -2,9 +2,45 @@
     {{-- The Master doesn't talk, he acts. --}}
     <ul class="space-y-1.5">
         {{-- jomblo active --}}
-        <x-bit.menu-item href="{{ route('home') }}" icon="house-simple" title="Dashboard" :active="request()->routeIs('home')" />
-        <x-bit.menu-item href="{{ route('about') }}" icon="address-book" title="About" :active="request()->routeIs('about')" />
-        <x-bit.menu-item type="sub" icon="users" title="Users" :active="request()->routeIs('users')" />
-        <x-bit.menu-item type="sub" icon="nut" :title="'app.previous'" :active="request()->routeIs('users')" :submenu=[ ] />
+        <x-bit.menu-item href="{{ route('home') }}" icon="house-simple" :title="__('app.dashboard')" :active="request()->routeIs('home')" />
+        <x-bit.menu-item href="{{ route('about') }}" icon="address-book" :title="__('app.about')" :active="request()->routeIs('about')" />
+        <x-bit.menu-item type="sub" icon="users" :title="__('app.users')" :active="request()->routeIs('users')" />
+        <x-bit.menu-item type="sub" icon="nut" :title="__('app.settings')" :active="request()->routeIs('users')" :submenu="[
+            [
+                'title' => 'Profile',
+                'href' => route('about'),
+                'active' => request()->routeIs('about'),
+                'child' => [],
+            ],
+        
+            [
+                'title' => 'submenu',
+                'href' => '#',
+                'active' => false,
+                'child' => [
+                    [
+                        'title' => 'Submenu 1',
+                        'href' => route('about'),
+                        'active' => request()->routeIs('about'),
+                    ],
+                    [
+                        'title' => 'Submenu 2',
+                        'href' => route('about'),
+                        'active' => request()->routeIs('about'),
+                    ],
+                    [
+                        'title' => 'Submenu 3',
+                        'href' => route('about'),
+                        'active' => request()->routeIs('about'),
+                    ],
+                ],
+            ],
+            [
+                'title' => 'Password',
+                'href' => route('about'),
+                'active' => request()->routeIs('about'),
+                'child' => [],
+            ],
+        ]" />
     </ul>
 </div>
