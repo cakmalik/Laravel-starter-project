b35d423768f03baaec214e82c119fff02a7b0c07
cakmalik <hasinilmalik@gmail.com>
2023-09-08 10:03:21 +0700

    add localization

--- lang/id/app.php
+++ lang/id/app.php
@@ -1,19 +1,12 @@
 <?php
 
 return [
-
-    /*
-        |--------------------------------------------------------------------------
-        | Livewire Language Lines
-        |--------------------------------------------------------------------------
-        |
-        | The following language lines are used by the Livewire library to build
-        | the simple pagination links. You are free to change them to anything
-        | you want to customize your views to better match your application.
-        |
-        */
-
-    'previous' => '&laquo; Sebelumnya',
-    'next' => 'Selanjutnya &raquo;',
-
+    'settings' => 'pengaturan',
+    'users' => 'pengguna',
+    'roles' => 'peran',
+    'logout' => 'keluar',
+    'about' => 'tentang',
+    'home' => 'beranda',
+    'search' => 'cari',
+    'dashboard' => 'dasboard',
 ];
